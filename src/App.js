95c6cf66738c0346974a63c2d08572f75ba54a6a
abdurrahman <programmer.abdurrahman@gmail.com>
2023-11-06 22:33:51 +0600

    competed assignment

--- src/App.js
+++ src/App.js
@@ -1,54 +1,99 @@
 import './App.css';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { AiFillPicture } from "react-icons/ai"
-import img2 from '../src/assets/image-2.webp';
-import img11 from '../src/assets/image-1.webp';
-import img3 from '../src/assets/image-3.webp';
-import img4 from '../src/assets/image-4.webp';
-import img5 from '../src/assets/image-5.webp';
-import img6 from '../src/assets/image-6.webp';
-import img7 from '../src/assets/image-7.webp';
-import img8 from '../src/assets/image-8.webp';
-import img9 from '../src/assets/image-9.webp';
-import img10 from '../src/assets/image-10.jpeg';
-import img1 from '../src/assets/image-11.jpeg';
 import { useEffect, useState } from 'react';
+import { getItems } from './utils/data';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+
+
 
 
 function App() {
-  const [selectedItem, setSelectedItem] = useState(img1);
-  const [items, setItems] = useState([img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]);
+  const [items, setItems] = useState(getItems);
+  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [checkedItems, setCheckedItems] = useState([])
+
+
+  useEffect(() => {
+    const checkItem = items.filter(item => item.isChecked && item)
+    setCheckedItems(checkItem)
+  }, [items])
+
 
   const hangleSelectedItem = (item) => {
     setSelectedItem(item);
   };
 
+
+  const handelDeleteItems = () => {
+    const updatedItems = items.filter(item => !checkedItems.includes(item));
+    setItems(updatedItems);
+    setCheckedItems([]);
+    setSelectedItem(updatedItems[0])
+    console.log(updatedItems)
+  }
+
+  const handleSelectItems = (e, id) => {
+    const updatedItems = items.map(item => {
+      if (item.id === id) {
+        return { ...item, isChecked: e.target.checked };
+      }
+      return item;
+    });
+
+    setItems(updatedItems);
+
+    console.log(updatedItems);
+  };
+
+
   const handleImageUpload = (event) => {
-    const file = event.target.files[0]; // Get the first selected file
+    const file = event.target.files[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
-      setItems((prevItems) => [...prevItems, imageUrl]);
-      console.log(items); // This will still show the previous value of items
+      const d = new Date();
+      let time = d.getTime();
+      console.log('Adding new item:', { id: items.length + 1, img: imageUrl, views: 12, time: time });
+      setItems((prevItems) => [...prevItems, { id: items.length + 1, img: imageUrl, views: 12, time: time }]);
+    }
+  };
+
+
+  const handleSortByLatest = (info) => {
+    console.log(info)
+    if (info == 'popularity') {
+      const sortedItems = [...items].sort((a, b) => b.views - a.views);
+      setItems(sortedItems);
+    } else if (info == 'latest') {
+      const sortedItems = [...items].sort((a, b) => b.time - a.time);
+      setItems(sortedItems);
     }
   };
 
 
   return (
     <div className='w-screen min-h-screen flex justify-center items-center bg-[#F7F5F2]' >
-      <div className='border-[10px] border-[#EDF2F7] w-full mx-auto max-w-[860px] min-h-[600px]'>
+      <div className='border-[10px] border-[#EDF2F7] w-full mx-auto max-w-[860px]'>
         <div className='h-full w-full bg-white rounded-md'>
           <div className='flex items-center justify-between px-[28px] py-[16px] border-b '>
             <div className="form-control">
               <label className="text-[14px] sm:ext-[16px] font-semibold cursor-pointer flex items-center gap-[12px]">
-                <input type="checkbox" className="checkbox checkbox-xs rounded-none checkbox-primary" />
-                <span className=" text-[14px] sm:text-[16px] font-semibold">Seleted Items</span>
+                <input type="checkbox" checked={checkedItems.length} onChange={e => { }} className="checkbox checkbox-xs rounded-none checkbox-primary" />
+                <span className=" text-[14px] sm:text-[16px] font-semibold">{checkedItems.length} Seleted Items</span>
               </label>
             </div>
-            <div className="form-control">
-              <label className="text-[14px] sm:text-[16px] font-semibold cursor-pointer flex items-center gap-[12px]">
-                <span className="text-[14px] sm:text-[16px] font-semibold">Delete Items</span>
-                <RiDeleteBin6Line className='text-[20px] hover:text-primary' />
-              </label>
+            <div className='flex items-center short-delete-parent gap-[20px]'>
+              <div className="form-control min-w-[130px] delete-item">
+                <label onClick={() => handelDeleteItems()} className="text-[14px] sm:text-[16px] font-semibold cursor-pointer flex items-center gap-[12px]">
+                  <span className="text-[14px] sm:text-[16px] font-semibold">Delete Items</span>
+                  <RiDeleteBin6Line className='text-[20px] hover:text-primary' />
+                </label>
+              </div>
+              <select className="select select-bordered w-full max-w-xs short-item" defaultValue="popularity" onChange={e => handleSortByLatest(e.target.value)}>
+                <option disabled hidden>Sort by</option>
+                <option value="popularity">popularity</option>
+                <option value="latest">latest</option>
+              </select>
             </div>
           </div>
           <div className='p-[28px]'>
@@ -58,28 +103,33 @@ function App() {
                   <figure className='rounded-md border relative item border-gray-100'>
                     <div className='absolute bg-animate bg-animate-1 w-full bg-[#8282824a]'></div>
                     <div className='absolute bg-animate bg-animate-2 w-full bg-[#8282824a]'></div>
-                    <img src={selectedItem} className='rounded-md' style={{ objectFit: 'cover', objectPosition: 'center cener' }} alt="seleted item" />
+                    <img src={selectedItem.img} className='rounded-md' style={{ objectFit: 'cover', objectPosition: 'center cener' }} alt="seleted item" />
                   </figure>
                 </div>
               </div>
               {
-                items.map((item, index) => <figure className='border relative rounded-md cursor-pointer item border-gray-100' key={index} onClick={() => hangleSelectedItem(item)}>
-                  <div className='absolute bg-animate bg-animate-1 w-full bg-[#8282824a]'></div>
-                  <div className='absolute bg-animate bg-animate-2 w-full bg-[#8282824a]'></div>
-                  <div className='absolute left-[10px] top-[10px] hidden select-checkbox'>
-                    <input type="checkbox" className="checkbox checkbox-xs rounded-none checkbox-primary bg-white" onClick={(e) => e.stopPropagation()} />
-                  </div>
-                  <div className=' rounded-md'  >
-                    <img src={item} alt="item" style={{ objectFit: 'cover', objectPosition: 'center cener' }} />
-                  </div>
-                </figure>)
+                items.map((item, index) => {
+                  return (
+                    <figure className='border relative rounded-md cursor-pointer item border-gray-100' key={index} onClick={() => hangleSelectedItem(item)}>
+                      <div className={`absolute bg-animate bg-animate-1 w-full bg-[#8282824a] ${item.isChecked ? 'checked' : ''}`}></div>
+                      <div className={`absolute bg-animate bg-animate-1 w-full bg-[#8282824a] ${item.isChecked ? 'checked' : ''}`}></div>
+                      <div className={`absolute left-[10px] top-[10px] hidden select-checkbox ${item.isChecked ? 'checked-box' : ''}`}>
+                        <input type="checkbox" checked={item.isChecked} className={`checkbox checkbox-xs rounded-none checkbox-primary bg-white `}
+                          onChange={e => { }} onClick={(e) => { e.stopPropagation(); handleSelectItems(e, item.id) }} />
+                      </div>
+                      <div className=' rounded-md'  >
+                        <img src={item?.img} alt="item" style={{ objectFit: 'cover', objectPosition: 'center cener' }} />
+                      </div>
+                    </figure>
+                  )
+                })
               }
               <label htmlFor='upload-image' className='border rounded-md relative item cursor-pointer text-[16px] flex flex-col py-[40px] items-center justify-center gap-20px font-semibold  border-dashed border-gray-200'>
                 <div className='absolute bg-animate bg-animate-1 w-full bg-[#8282824a]'></div>
                 <div className='absolute bg-animate bg-animate-2 w-full bg-[#8282824a]'></div>
                 <AiFillPicture />
                 Add images
-                <input type="file" onChange={handleImageUpload} className='hidden' id='upload-image' />
+                <input type="file" onChange={(e) => handleImageUpload(e)} className='hidden' id='upload-image' />
               </label>
             </div>
           </div>
---
+++ src/utils/data.js
@@ -0,0 +1,23 @@
+import img2 from '../assets/image-2.webp';
+import img3 from '../assets/image-3.webp';
+import img4 from '../assets/image-4.webp';
+import img5 from '../assets/image-5.webp';
+import img6 from '../assets/image-6.webp';
+import img7 from '../assets/image-7.webp';
+import img8 from '../assets/image-8.webp';
+import img9 from '../assets/image-9.webp';
+import img10 from '../assets/image-10.jpeg';
+import img1 from '../assets/image-11.jpeg';
+
+export let getItems = [
+    { id: 1, img: img1, isChecked: false, views: 50, time: 699284917555, },
+    { id: 2, img: img2, isChecked: false, views: 3, time: 1699284961771 },
+    { id: 3, img: img3, isChecked: false, views: 2, time: 1699284973232 },
+    { id: 4, img: img4, isChecked: false, views: 23, time: 1699285037707 },
+    { id: 5, img: img5, isChecked: false, views: 15, time: 1699285037707 },
+    { id: 6, img: img6, isChecked: false, views: 12, time: 1699285025927 },
+    { id: 7, img: img7, isChecked: false, views: 16, time: 1699284985791 },
+    { id: 8, img: img8, isChecked: false, views: 18, time: 1699284998426 },
+    { id: 9, img: img9, isChecked: false, views: 12, time: 1699285012537 },
+    { id: 10, img: img10, isChecked: false, views: 12, time: 1699284985791 },
+]
